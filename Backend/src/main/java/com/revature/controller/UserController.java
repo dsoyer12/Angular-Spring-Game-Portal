03980fb0d442eecee0f614e880a3552ba343de7e
Djourn G.A. Gordon-Taylor Soyer <dsoyer12@gmail.com>
2019-10-14 21:28:51 -0400

    pulled from backend

--- Backend/src/main/java/com/revature/controller/UserController.java
+++ Backend/src/main/java/com/revature/controller/UserController.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.User;
 import com.revature.service.P2Services;
 
 @Controller
+@RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/user")
 public class UserController {
 
