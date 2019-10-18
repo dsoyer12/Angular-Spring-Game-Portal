6f5dc4eeadcd04c07343981d4504c165496845fc
Raymond Hua <raymondjhua@gmail.com>
2019-10-18 18:03:27 -0400

    Full backend with JUnit test

--- Backend/src/main/java/com/revature/service/P2ServiceTest.java
+++ Backend/src/main/java/com/revature/service/P2ServiceTest.java
@@ -1,101 +1,84 @@
-//package com.revature.service;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import java.util.List;
-//
-//import org.hibernate.SessionFactory;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestExecutionListeners;
-//
-//import com.revature.dao.P2DAOImpl;
-//import com.revature.model.*;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations={"Student-servlet.xml"})
-//@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
-//public class P2ServiceTest {
-//	
-//	@Autowired
-//	private P2DAOImpl dao;
-//
-//	@Test
-//	public void testAuthenticate() {
-//		System.out.println("Assert Raymond, Hua is a user");
-//		User test = dao.Authenticate(new User("Raymond", "Hua"));
-//		boolean isThere = false;
-//		if (test.getUser_id() == 1)
-//			isThere = true;
-//		isThere = true;
-//		assertEquals(isThere, true);
-//	}
-//
-//	@Test
-//	public void testAddUser() {
-//		System.out.println("Add John, Doe as a user");
-//		dao.addUser(new User ("John","Doe"));
-//		List<User> test = dao.getAllUsers();
-//		boolean isThere = false;
-//		for (User i : test) {
-//			if (i.getUsername().equals("John") && i.getPassword().equals("Doe"))
-//				isThere = true;
-//		}
-//		assertEquals(isThere, true);
-//	}
-//
-//	@Test
-//	public void testAddGame() {
-//		System.out.println("Add Pinball as a game");
-//		dao.addGame(new Game ("Pinball"));
-//		List<Game> test = dao.getAllGames();
-//		boolean isThere = false;
-//		for (Game i : test) {
-//			if (i.getDescription().equals("Pinball"))
-//				isThere = true;
-//		}
-//		assertEquals(isThere, true);
-//	}
-//	
-//	@Test
-//	public void testAddScore() {
-//		System.out.println("Add score of 100, as a user John Doe, in game 2");
-//		dao.addScore(new Score (100, dao.Authenticate(new User ("John", "Doe")), new Game (2, "")));
-//		List<Score> test = dao.getAllScores();
-//		boolean isThere = false;
-//		for (Score i : test) {
-//			if (i.getScores() == 100 && i.getUser().getUsername().equals("John") && i.getUser().getPassword().equals("Doe") && i.getGame().getId() == 2)
-//				isThere = true;
-//		}
-//		assertEquals(isThere, true);
-//	}
-//	
-//	@Test
-//	public void testAddWin() {
-//		System.out.println("Add a win, as a user John Doe, in game 1");
-//		dao.addWin(new Win (1, dao.Authenticate(new User ("John", "Doe")), new Game (1, "")));
-//		List<Win> test = dao.getAllWins();
-//		boolean isThere = false;
-//		for (Win i : test) {
-//			if (i.getCount() == 1 && i.getUser().getUsername().equals("John") && i.getUser().getPassword().equals("Doe") && i.getGame().getId() == 2)
-//				isThere = true;
-//		}
-//		assertEquals(isThere, true);
-//	}
-//	
-//	@Test
-//	public void testUpdateWin() {
-//		System.out.println("Add a win, as a user John Doe, in game 1");
-//		dao.updateWin(new Win (1, dao.Authenticate(new User ("John", "Doe")), new Game (1, "")));
-//		List<Win> test = dao.getAllWins();
-//		boolean isThere = false;
-//		for (Win i : test) {
-//			if (i.getCount() == 2 && i.getUser().getUsername().equals("John") && i.getUser().getPassword().equals("Doe") && i.getGame().getId() == 2)
-//				isThere = true;
-//		}
-//		assertEquals(isThere, true);
-//	}
-//}
+package com.revature.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.revature.OrmConfiguration;
+import com.revature.model.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = OrmConfiguration.class)
+public class P2ServiceTest {
+
+	private P2Services service;
+	
+	@Autowired
+	public void setCredential(P2Services service) {
+		this.service = service;
+	}
+
+	@Test
+	public void testAuthenticate() {
+		User test = service.Authenticate(new User ("Raymond", "Hua"));
+		assertEquals(test.getUser_id(), 1);
+	}
+	
+	@Test
+	public void testTop10Scores() {
+		List<Score> test = service.top10Scores(new Game(2, ""));
+		assertEquals(test.size(), 10);
+	}
+	
+	@Test
+	public void testUserScores() {
+		List<Score> test = service.getUserScores(new User (1, "Raymond", "Hua"), new Game(2, ""));
+		boolean isUser = true;
+		for (Score i : test)
+			if(i.getUser().getUser_id() != 1)
+				isUser = false;
+		assertEquals(isUser, true);
+	}
+
+	@Test
+	public void testGetUserWins() {
+		List<Win> test = service.getUserWins(new User (1, "Raymond", "Hua"), new Game(3, ""));
+		boolean isUser = true;
+		for (Win i : test)
+			if(i.getUser().getUser_id() != 1)
+				isUser = false;
+		assertEquals(isUser, true);
+	}
+	
+	@Test
+	public void testUpdateWins() {
+		List<Win> test = service.getUserWins(new User (1, "Raymond", "Hua"), new Game(3, ""));
+		Win tester = test.get(0);
+		int previous = tester.getCount();
+		service.updateWin(new Win (0, new User (1, "Raymond", "Hua"), new Game(3, "")));
+		test = service.getUserWins(new User (1, "Raymond", "Hua"), new Game(3, ""));
+		tester = test.get(0);
+		int current = tester.getCount();
+		assertEquals(previous, current-1);
+	}
+	
+	@Test
+	public void testAddUser() {
+		service.addUser(new User ("Emily", "Higgins"));
+		List<User> test = service.getAllUsers();
+		boolean isUser = false;
+		for (User i : test)
+			if(i.getUsername().equals("Emily") && i.getPassword().equals("Higgins")) {
+				isUser = true;
+				break;
+			}
+		assertEquals(isUser, true);
+	}
+}
